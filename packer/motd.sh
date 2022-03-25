0804f4532788341bf5267343e467b6195c7185f6
wim-vandijck-sp <wim.vandijck@sailpoint.com>
2022-03-25 10:01:20 -0400

    Better motd write

--- packer/motd.sh
+++ packer/motd.sh
@@ -1,19 +1,20 @@
 #!/bin/sh -eux
 
 # https://patorjk.com/software/taag/#p=display&f=Big&t=SERI%20Gibraltar
-seri="
+seri=$(cat <<'END_HEREDOC'
    _____ ______ _____  _____    _____ _ _               _ _             
   / ____|  ____|  __ \|_   _|  / ____(_) |             | | |            
  | (___ | |__  | |__) | | |   | |  __ _| |__  _ __ __ _| | |_ __ _ _ __ 
   \___ \|  __| |  _  /  | |   | | |_ | | '_ \| '__/ _` | | __/ _` | '__|
   ____) | |____| | \ \ _| |_  | |__| | | |_) | | | (_| | | || (_| | |   
  |_____/|______|_|  \_\_____|  \_____|_|_.__/|_|  \__,_|_|\__\__,_|_|   
-"
+
 
 Welcome to the SE Reference Implementation! [SERI v. Gibraltar]
 
 For a general documentation, please visit http://seri.saiplointdemo.com.'
-
+END_HEREDOC
+)
 if [ -d /etc/update-motd.d ]; then
     MOTD_CONFIG='/etc/update-motd.d/99-seri'
 
